4534bac0e167878b93ef11722916cdfd150b475c
Luke Wagener <lukewagener1@gmail.com>
2023-10-06 15:26:50 -0500

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+require 'csv'
+require 'json'
+
+# AIMLsalaries Dataset
+CSV.foreach(Rails.root.join('lib/seeds/aiml_salaries.csv'), headers: true) do |row|
+  AimlSalary.create! row.to_hash
+end
+
+# Global Electricity Statistics Dataset
+CSV.foreach(Rails.root.join('lib/seeds/global_electricity_statistics.csv'), headers: true) do |row|
+  GlobalElectricityStatistic.create! row.to_hash
+end
+
+# WorldBankCountries Dataset
+CSV.foreach(Rails.root.join('lib/seeds/world_bank_countries.csv'), headers: true) do |row|
+  WorldBankCountry.create! row.to_hash
+end
+
+# Countries Names Dataset
+countries_names_json = File.read(Rails.root.join('lib/seeds/countries_names.json'))
+countries_names_hash = JSON.parse(countries_names_json)
+countries_names_hash.each do |code, name|
+  CountryName.create!(country_code: code, country_name: name)
+end
