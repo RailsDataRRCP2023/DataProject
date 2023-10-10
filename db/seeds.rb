c6d0afdbf54d3e8eb904f2fc52bebd3f6aa59558
DZ-FSDev <dz.uofm@gmail.com>
2023-10-10 12:32:46 -0500

    Refactor Seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,22 +4,22 @@ require 'csv'
 require 'json'
 
 # AIMLsalaries Dataset
-CSV.foreach(Rails.root.join('lib/seeds/aiml_salaries.csv'), headers: true) do |row|
-  AimlSalary.create! row.to_hash
+CSV.foreach('db/aiml_salaries.csv', headers: true) do |row|
+  AimlSalary.create! row.to_hash.select { |k, _| AimlSalary.column_names.include?(k) }.to_hash
 end
 
 # Global Electricity Statistics Dataset
-CSV.foreach(Rails.root.join('lib/seeds/global_electricity_statistics.csv'), headers: true) do |row|
-  GlobalElectricityStatistic.create! row.to_hash
+CSV.foreach('db/global_electricity_statistics.csv', headers: true) do |row|
+  GlobalElectricityStatistic.create! row.to_hash.select { |k, _| GlobalElectricityStatistic.column_names.include?(k) }.to_hash
 end
 
 # WorldBankCountries Dataset
-CSV.foreach(Rails.root.join('lib/seeds/world_bank_countries.csv'), headers: true) do |row|
-  WorldBankCountry.create! row.to_hash
+CSV.foreach('db/world_bank_countries.csv', headers: true) do |row|
+  WorldBankCountry.create! row.to_hash.select { |k, _| WorldBankCountry.column_names.include?(k) }.to_hash
 end
 
 # Countries Names Dataset
-countries_names_json = File.read(Rails.root.join('lib/seeds/countries_names.json'))
+countries_names_json = File.read('db/Countries_Names.json')
 countries_names_hash = JSON.parse(countries_names_json)
 countries_names_hash.each do |code, name|
   CountryName.create!(country_code: code, country_name: name)
