54b0ceb60d3acf363120c2519232a638cc679691
DZ-FSDev <dz.uofm@gmail.com>
2023-10-10 12:31:20 -0500

    Update 20231006202914_create_global_electricity_statistics.rb

--- db/migrate/20231006202914_create_global_electricity_statistics.rb
+++ db/migrate/20231006202914_create_global_electricity_statistics.rb
@@ -4,12 +4,48 @@ class CreateGlobalElectricityStatistics < ActiveRecord::Migration[7.0]
       t.string :Country
       t.string :Features
       t.string :Region
-      t.decimal :1980
-      t.decimal :1981
-      t.decimal :1982
-      t.decimal :1983
-      t.string :...
-      t.decimal :2021
+      t.decimal :_1980
+      t.decimal :_1981
+      t.decimal :_1982
+      t.decimal :_1983
+      t.decimal :_1984
+      t.decimal :_1985
+      t.decimal :_1986
+      t.decimal :_1987
+      t.decimal :_1988
+      t.decimal :_1989
+      t.decimal :_1990
+      t.decimal :_1991
+      t.decimal :_1992
+      t.decimal :_1993
+      t.decimal :_1994
+      t.decimal :_1995
+      t.decimal :_1996
+      t.decimal :_1997
+      t.decimal :_1998
+      t.decimal :_1999
+      t.decimal :_2000
+      t.decimal :_2001
+      t.decimal :_2002
+      t.decimal :_2003
+      t.decimal :_2004
+      t.decimal :_2005
+      t.decimal :_2006
+      t.decimal :_2007
+      t.decimal :_2008
+      t.decimal :_2009
+      t.decimal :_2010
+      t.decimal :_2011
+      t.decimal :_2012
+      t.decimal :_2013
+      t.decimal :_2014
+      t.decimal :_2015
+      t.decimal :_2016
+      t.decimal :_2017
+      t.decimal :_2018
+      t.decimal :_2019
+      t.decimal :_2020
+      t.decimal :_2021
 
       t.timestamps
     end
