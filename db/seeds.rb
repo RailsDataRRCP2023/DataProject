bf242f052c783c54890618d496a229b5c0d4fc54
DZ-FSDev <dz.uofm@gmail.com>
2023-10-11 11:00:00 -0600

    Seeds to failforward

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,23 +4,43 @@ require 'csv'
 require 'json'
 
 # AIMLsalaries Dataset
+pp 'AIMLsalaries Dataset'
 CSV.foreach('db/aiml_salaries.csv', headers: true) do |row|
-  AimlSalary.create! row.to_hash.select { |k, _| AimlSalary.column_names.include?(k) }.to_hash
+    begin
+        AimlSalary.create! row.to_hash.select { |k, _| AimlSalary.column_names.include?(k) }.to_hash
+    rescue => exception
+        pp exception
+    end
 end
 
 # Global Electricity Statistics Dataset
+pp 'Global Electricity Statistics Dataset'
 CSV.foreach('db/global_electricity_statistics.csv', headers: true) do |row|
-  GlobalElectricityStatistic.create! row.to_hash.select { |k, _| GlobalElectricityStatistic.column_names.include?(k) }.to_hash
+    begin
+        GlobalElectricityStatistic.create! row.to_hash.select { |k, _| GlobalElectricityStatistic.column_names.include?(k) }.to_hash
+    rescue => exception
+        pp exception        
+    end
 end
 
 # WorldBankCountries Dataset
+pp 'WorldBankCountries Dataset'
 CSV.foreach('db/world_bank_countries.csv', headers: true) do |row|
-  WorldBankCountry.create! row.to_hash.select { |k, _| WorldBankCountry.column_names.include?(k) }.to_hash
+    begin
+        WorldBankCountry.create! row.to_hash.select { |k, _| WorldBankCountry.column_names.include?(k) }.to_hash
+    rescue => exception
+        pp exception
+    end
 end
 
 # Countries Names Dataset
+pp 'Countries Names Dataset'
 countries_names_json = File.read('db/Countries_Names.json')
 countries_names_hash = JSON.parse(countries_names_json)
 countries_names_hash.each do |code, name|
-  CountryName.create!(country_code: code, country_name: name)
+    begin
+        CountryName.create!(country_code: code, country_name: name)
+    rescue => exception
+        pp exception
+    end
 end
