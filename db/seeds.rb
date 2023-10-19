df9d5c6d9ded052250402d473885c90bd544ed5a
DZ-FSDev <dz.uofm@gmail.com>
2023-10-19 15:06:55 -0500

    refactor seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,24 +18,19 @@ countries_names_hash.each do |code, name|
     begin
         CountryName.create!(country_code: code, country_name: name)
     rescue => exception
-        pp exception
+        pp 'Countries Names Dataset: ' + exception.to_s
     end
 end
-
-# Clear all tables
-pp 'Clear all tables'
-AimlSalary.destroy_all
-GlobalElectricityStatistic.destroy_all
-WorldBankCountry.destroy_all
-CountryName.destroy_all
 
 # AIMLsalaries Dataset
 pp 'AIMLsalaries Dataset'
 CSV.foreach('db/aiml_salaries.csv', headers: true) do |row|
     begin
-        AimlSalary.create! row.to_hash.select { |k, _| AimlSalary.column_names.include?(k) }.to_hash
+        record = row.to_hash
+        record['country_name_id'] = CountryName.find_by(country_code: record['company_location']).id
+        AimlSalary.create! record.select { |k, _| AimlSalary.column_names.include?(k) }.to_hash
     rescue => exception
-        pp exception
+        pp 'AIMLsalaries Dataset: ' + exception.to_s
     end
 end
 
@@ -43,9 +38,12 @@ end
 pp 'Global Electricity Statistics Dataset'
 CSV.foreach('db/global_electricity_statistics.csv', headers: true) do |row|
     begin
-        GlobalElectricityStatistic.create! row.to_hash.select { |k, _| GlobalElectricityStatistic.column_names.include?(k) }.to_hash
+        record = row.to_hash
+        record['country'] = record['country'].strip
+        record['country_name_id'] = CountryName.find_by(country_code: record['country']).id
+        GlobalElectricityStatistic.create! record.select { |k, _| GlobalElectricityStatistic.column_names.include?(k) }.to_hash
     rescue => exception
-        pp exception        
+        pp 'Global Electricity Statistics Dataset: ' + exception.to_s
     end
 end
 
@@ -53,9 +51,11 @@ end
 pp 'WorldBankCountries Dataset'
 CSV.foreach('db/world_bank_countries.csv', headers: true) do |row|
     begin
-        WorldBankCountry.create! row.to_hash.select { |k, _| WorldBankCountry.column_names.include?(k) }.to_hash
+        record = row.to_hash
+        record['country_name_id'] = CountryName.find_by(country_code: record['country_name']).id
+        WorldBankCountry.create! record.select { |k, _| WorldBankCountry.column_names.include?(k) }.to_hash
     rescue => exception
-        pp exception
+        pp 'WorldBankCountries Dataset: ' + exception.to_s
     end
 end
 
