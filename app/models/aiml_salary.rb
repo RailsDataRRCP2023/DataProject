a91e2e7230dfe129a796b2f7e644188f56653677
Luke Wagener <lukewagener1@gmail.com>
2023-10-20 10:57:47 -0500

    Update aiml_salary.rb

--- app/models/aiml_salary.rb
+++ app/models/aiml_salary.rb
@@ -1,5 +1,5 @@
 class AimlSalary < ApplicationRecord
-    validates :work_year, :experience_level, :employment_type, :job_title, :salary, :salary_currency, :salary_in_usd, :employee_residence, :remote_ratio, :company_location, :company_size, presence: true
+    validates :work_year, :experience_level, :employment_type, :job_title, :salary, :salary_currency, :salary_in_usd, :remote_ratio, :company_size, presence: true
     
-    belongs_to :country_name, foreign_key: 'company_location', primary_key: 'country_name'
+    belongs_to :country_name
   end
