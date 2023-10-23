83b33464a19f1ac60d9e8035dd9676d592df34bd
Luke Wagener <lukewagener1@gmail.com>
2023-10-23 13:30:52 -0500

    Bootstrap Card format to member pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,58 +5,59 @@ require 'json'
 
 # Clear all tables
 pp 'Clear all tables'
-AimlSalary.destroy_all
+# AimlSalary.destroy_all
 GlobalElectricityStatistic.destroy_all
 WorldBankCountry.destroy_all
-CountryName.destroy_all
+# CountryName.destroy_all
 
-# Countries Names Dataset
-pp 'Countries Names Dataset'
-countries_names_json = File.read('db/Countries_Names.json')
-countries_names_hash = JSON.parse(countries_names_json)
-countries_names_hash.each do |code, name|
-    begin
-        CountryName.create!(country_code: code, country_name: name)
-    rescue => exception
-        pp 'Countries Names Dataset: ' + exception.to_s
-    end
-end
+# # Countries Names Dataset
+# pp 'Countries Names Dataset'
+# countries_names_json = File.read('db/Countries_Names.json')
+# countries_names_hash = JSON.parse(countries_names_json)
+# countries_names_hash.each do |code, name|
+#     begin
+#         CountryName.create!(country_code: code, country_name: name)
+#     rescue => exception
+#         pp 'Countries Names Dataset: ' + exception.to_s
+#     end
+# end
 
-# AIMLsalaries Dataset
-pp 'AIMLsalaries Dataset'
-CSV.foreach('db/aiml_salaries.csv', headers: true) do |row|
-    begin
-        record = row.to_hash
-        record['country_name_id'] = CountryName.find_by(country_code: record['company_location']).id
-        AimlSalary.create! record.select { |k, _| AimlSalary.column_names.include?(k) }.to_hash
-    rescue => exception
-        pp 'AIMLsalaries Dataset: ' + exception.to_s
-    end
-end
+# # AIMLsalaries Dataset
+# pp 'AIMLsalaries Dataset'
+# CSV.foreach('db/aiml_salaries.csv', headers: true) do |row|
+#     begin
+#         record = row.to_hash
+#         record['country_name_id'] = CountryName.find_by(country_code: record['company_location']).id
+#         AimlSalary.create! record.select { |k, _| AimlSalary.column_names.include?(k) }.to_hash
+#     rescue => exception
+#         pp 'AIMLsalaries Dataset: ' + exception.to_s
+#     end
+# end
 
 # Global Electricity Statistics Dataset
 pp 'Global Electricity Statistics Dataset'
 CSV.foreach('db/global_electricity_statistics.csv', headers: true) do |row|
     begin
         record = row.to_hash
-        record['country'] = record['country'].strip
-        record['country_name_id'] = CountryName.find_by(country_code: record['country']).id
+        pp(record)
+        record['Country'] = record['Country'].strip
+        record['country_name_id'] = CountryName.find_by(country_code: record['Country']).id
         GlobalElectricityStatistic.create! record.select { |k, _| GlobalElectricityStatistic.column_names.include?(k) }.to_hash
     rescue => exception
         pp 'Global Electricity Statistics Dataset: ' + exception.to_s
     end
 end
 
-# WorldBankCountries Dataset
-pp 'WorldBankCountries Dataset'
-CSV.foreach('db/world_bank_countries.csv', headers: true) do |row|
-    begin
-        record = row.to_hash
-        record['country_name_id'] = CountryName.find_by(country_code: record['country_name']).id
-        WorldBankCountry.create! record.select { |k, _| WorldBankCountry.column_names.include?(k) }.to_hash
-    rescue => exception
-        pp 'WorldBankCountries Dataset: ' + exception.to_s
-    end
-end
+# # WorldBankCountries Dataset
+# pp 'WorldBankCountries Dataset'
+# CSV.foreach('db/world_bank_countries.csv', headers: true) do |row|
+#     begin
+#         record = row.to_hash
+#         record['country_name_id'] = CountryName.find_by(country_code: record['country_name']).id
+#         WorldBankCountry.create! record.select { |k, _| WorldBankCountry.column_names.include?(k) }.to_hash
+#     rescue => exception
+#         pp 'WorldBankCountries Dataset: ' + exception.to_s
+#     end
+# end
 
 
